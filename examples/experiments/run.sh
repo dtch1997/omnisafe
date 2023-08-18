dd2614fc68721727b151016e5b138bcffc285d86
Daniel Tan <dtch1997@users.noreply.github.com>
2023-08-18 11:59:52 +0100

    update experiments

--- examples/experiments/run.sh
+++ examples/experiments/run.sh
@@ -1,20 +1,28 @@
 
 #!/bin/bash
 
-# The variable $1 refers to the first argument passed to the script. 
-# If no argument is passed, 'Default Value' will be used.
-seed=${1:-'0'}
-group_name=${2:-'sac_baselines'}
+group_name=${1:-'safety-violation-expt'}
 
-for env in SafetyPointGoal1-v0 SafetyDoggoGoal1-v0 SafetyCarGoal1-v0
+for seed in 0 1 2
 do 
-    for algo in SAC SACLag
-    do
-        python examples/train_policy.py \
-            --env-id $env \
-            --algo $algo \
-            --track \
-            --total-steps 1000000 \
-            --group-name $group_name
+    for task in Goal # Circle Push
+    do 
+        for robot in Point Car # Doggo
+        do
+            for level in 1 2
+            do 
+                for algo in PPO PPOLag SAC SACLag FOCOPS CPO 
+                do
+                    env_id="Safety${robot}${task}${level}-v0"
+                    python examples/train_policy.py \
+                        --env-id $env \
+                        --algo $algo \
+                        --track \
+                        --total-steps 1000000 \
+                        --wandb-group $group_name \
+                        --seed $seed
+                done
+            done
+        done
     done
 done
